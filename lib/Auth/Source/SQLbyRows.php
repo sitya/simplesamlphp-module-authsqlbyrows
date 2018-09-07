411ade3b30806109e508755cc33f05e4092ba262
Frank Tamás <sitya@niif.hu>
2018-09-07 09:18:36 +0200

    coding style

--- lib/Auth/Source/SQLbyRows.php
+++ lib/Auth/Source/SQLbyRows.php
@@ -1,139 +1,138 @@
 <?php
 
-class sspmod_authsqlbyrows_Auth_Source_SQLbyRows extends sspmod_core_Auth_UserPassBase {
+class sspmod_authsqlbyrows_Auth_Source_SQLbyRows extends sspmod_core_Auth_UserPassBase
+{
+    private $dsn;
 
+    private $username;
 
-	private $dsn;
+    private $password;
 
-	private $username;
+    private $query;
 
-	private $password;
+    private $password_hashing; # default: cleartext
 
-	private $query;
+    public function __construct($info, $config)
+    {
+        assert('is_array($info)');
+        assert('is_array($config)');
+        // Call the parent constructor first, as required by the interface
+        parent::__construct($info, $config);
+        // Make sure that all required parameters are present.
+        foreach (array('dsn', 'username', 'password', 'query', 'password_hashing') as $param) {
+            if (!array_key_exists($param, $config)) {
+                throw new Exception('Missing required attribute \'' . $param .
+                    '\' for authentication source ' . $this->authId);
+            }
+            if (!is_string($config[$param])) {
+                throw new Exception('Expected parameter \'' . $param .
+                    '\' for authentication source ' . $this->authId .
+                    ' to be a string. Instead it was: ' .
+                    var_export($config[$param], true));
+            }
+        }
+        $this->dsn = $config['dsn'];
+        $this->username = $config['username'];
+        $this->password = $config['password'];
+        $this->query = $config['query'];
+        $this->password_hashing = $config['password_hashing'];
+    }
 
-	private $password_hashing; # default: cleartext
+    private function connect()
+    {
+        try {
+            $db = new PDO($this->dsn, $this->username, $this->password);
+        } catch (PDOException $e) {
+            throw new Exception('sqlauth:' . $this->authId . ': - Failed to connect to \'' .
+                $this->dsn . '\': '. $e->getMessage());
+        }
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $driver = explode(':', $this->dsn, 2);
+        $driver = strtolower($driver[0]);
+        /* Driver specific initialization. */
+        switch ($driver) {
+            case 'mysql':
+                /* Use UTF-8. */
+                $db->exec("SET NAMES 'utf8'");
+                break;
+            case 'pgsql':
+                /* Use UTF-8. */
+                $db->exec("SET NAMES 'UTF8'");
+                break;
+        }
+        return $db;
+    }
 
-	public function __construct($info, $config) {
-		assert('is_array($info)');
-		assert('is_array($config)');
-		// Call the parent constructor first, as required by the interface
-		parent::__construct($info, $config);
-		// Make sure that all required parameters are present.
-		foreach (array('dsn', 'username', 'password', 'query', 'password_hashing') as $param) {
-			if (!array_key_exists($param, $config)) {
-				throw new Exception('Missing required attribute \'' . $param .
-					'\' for authentication source ' . $this->authId);
-			}
-			if (!is_string($config[$param])) {
-				throw new Exception('Expected parameter \'' . $param .
-					'\' for authentication source ' . $this->authId .
-					' to be a string. Instead it was: ' .
-					var_export($config[$param], TRUE));
-			}
-		}
-		$this->dsn = $config['dsn'];
-		$this->username = $config['username'];
-		$this->password = $config['password'];
-		$this->query = $config['query'];
-		$this->password_hashing = $config['password_hashing'];
-	}
+    protected function login($username, $password)
+    {
+        assert('is_string($username)');
+        assert('is_string($password)');
 
-	private function connect() {
-		try {
-			$db = new PDO($this->dsn, $this->username, $this->password);
-		} catch (PDOException $e) {
-			throw new Exception('sqlauth:' . $this->authId . ': - Failed to connect to \'' .
-				$this->dsn . '\': '. $e->getMessage());
-		}
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$driver = explode(':', $this->dsn, 2);
-		$driver = strtolower($driver[0]);
-		/* Driver specific initialization. */
-		switch ($driver) {
-		case 'mysql':
-			/* Use UTF-8. */
-			$db->exec("SET NAMES 'utf8'");
-			break;
-		case 'pgsql':
-			/* Use UTF-8. */
-			$db->exec("SET NAMES 'UTF8'");
-			break;
-		}
-		return $db;
-	}
+        $db = $this->connect();
 
-	protected function login($username, $password) {
-		assert('is_string($username)');
-		assert('is_string($password)');
+        try {
+            $sth = $db->prepare($this->query);
+        } catch (PDOException $e) {
+            throw new Exception('sqlauth:' . $this->authId .
+                ': - Failed to prepare query: ' . $e->getMessage());
+        }
 
-		$db = $this->connect();
+        try {
+            if ($this->password_hashing != 'cleartext') {
+                $password = hash($this->password_hashing, $password);
+            }
+            $res = $sth->execute(array('username' => $username, 'password' => $password));
+        } catch (PDOException $e) {
+            throw new Exception('sqlauth:' . $this->authId .
+                ': - Failed to execute query: ' . $e->getMessage());
+        }
 
-		try {
-			$sth = $db->prepare($this->query);
-		} catch (PDOException $e) {
-			throw new Exception('sqlauth:' . $this->authId .
-				': - Failed to prepare query: ' . $e->getMessage());
-		}
+        try {
+            $data = $sth->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception('sqlauth:' . $this->authId .
+                ': - Failed to fetch result set: ' . $e->getMessage());
+        }
 
-		try {
-			if ($this->password_hashing != 'cleartext') {
-				$password = hash($this->password_hashing, $password);
-			}
-			$res = $sth->execute(array('username' => $username, 'password' => $password));
-		} catch (PDOException $e) {
-			throw new Exception('sqlauth:' . $this->authId .
-				': - Failed to execute query: ' . $e->getMessage());
-		}
+        SimpleSAML\Logger::info('sqlauth:' . $this->authId . ': Got ' . count($data) .
+            ' rows from database');
 
-		try {
-			$data = $sth->fetchAll(PDO::FETCH_ASSOC);
-		} catch (PDOException $e) {
-			throw new Exception('sqlauth:' . $this->authId .
-				': - Failed to fetch result set: ' . $e->getMessage());
-		}
+        if (count($data) === 0) {
+            /* No rows returned - invalid username/password. */
+            SimpleSAML\Logger::error('sqlauth:' . $this->authId .
+                ': No rows in result set. Probably wrong username/password. Username: ' . $username);
+            throw new SimpleSAML_Error_Error('WRONGUSERPASS');
+        }
 
-		SimpleSAML_Logger::info('sqlauth:' . $this->authId . ': Got ' . count($data) .
-			' rows from database');
+        $attributes = array();
+        foreach ($data as $row) {
+            $name = $row['_key'];
+            $value  = $row['_value'];
 
-		if (count($data) === 0) {
-			/* No rows returned - invalid username/password. */
-			SimpleSAML_Logger::error('sqlauth:' . $this->authId .
-				': No rows in result set. Probably wrong username/password. Username: ' . $username);
-			throw new SimpleSAML_Error_Error('WRONGUSERPASS');
-		}
+            if ($value === null || $name == null) {
+                continue;
+            }
 
-		$attributes = array();
-		foreach ($data as $row) {
+            if (!array_key_exists($name, $attributes)) {
+                $attributes[$name] = array();
+            }
 
-			$name = $row['_key'];
-			$value  = $row['_value'];
+            $subvalues = explode(';', $value);
+            foreach ($subvalues as $subvalue) {
+                $subvalue = (string)$subvalue;
 
-			if ($value === null || $name == null) {
-				continue;
-			}			
+                if (in_array($subvalue, $attributes[$name], true)) {
+                    /* Value already exists in attribute. */
+                    continue;
+                }
 
-			if (!array_key_exists($name, $attributes)) {
-				$attributes[$name] = array();
-			}
+                $attributes[$name][] = trim($subvalue);
+            }
+        }
 
-			$subvalues = explode(';',$value);
-			foreach ($subvalues as $subvalue){
-			
-				$subvalue = (string)$subvalue;
+        SimpleSAML\Logger::info('sqlauth:' . $this->authId . ': Attributes: ' .
+            implode(',', array_keys($attributes)));
 
-				if (in_array($subvalue, $attributes[$name], true)) {
-					/* Value already exists in attribute. */
-					continue;
-				}
-
-				$attributes[$name][] = trim($subvalue);
-			}
-		}
-
-		SimpleSAML_Logger::info('sqlauth:' . $this->authId . ': Attributes: ' .
-			implode(',', array_keys($attributes)));
-
-		return $attributes;
-	}
-
+        return $attributes;
+    }
 }
